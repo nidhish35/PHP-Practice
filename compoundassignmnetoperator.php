4895894876eb8d87cf14e6850b1e9f34be976562
nidhish35 <malavnidhish35@gmail.com>
2025-03-18 09:30:49 +0530

    18 march php practice

---
+++ compoundassignmnetoperator.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $num2 = 100;
+        $num2 += 20;
+        echo "Increased Value: ",$num2;
+        $num2 -= 20;
+        echo "<br/> Decreased Value: ",$num2;
+        $num2 *= 20;
+        echo "<br/> Multiplied Value",$num2;
+    ?>
+</body>
+</html>
