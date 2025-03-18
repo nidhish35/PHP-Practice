fc46aba6a8d7683509d1a905cb6dd7446e2fbdc0
nidhish35 <malavnidhish35@gmail.com>
2025-03-18 09:21:21 +0530

    18 march php practice

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Example</title>
+</head>
+<body>
+    <h1>Welcome to My PHP Page</h1>
+    <p>
+        <?php
+            echo "Hello, World! This is a PHP script inside HTML.";
+        ?>
+    </p>
+</body>
+</html>
---
+++ sum.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SUM</title>
+</head>
+<body>
+    <h1>Sum of Two Numbers</h1>
+    <p>
+        <?php
+            $num1 = 10;
+            $num2 = 20;
+            $sum = $num1 + $num2;
+            echo "The sum of $num1 and $num2 is: $sum";
+            echo "<br>";
+            $sub = $num1 - $num2;
+            echo "The sub of $num1 and $num2 is: $sub";
+        ?>
+    </p>
+</body>
+</html>
