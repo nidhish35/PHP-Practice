f15df1d1d7f79427ad6fad5c1f19b6044a00eae3
nidhish35 <malavnidhish35@gmail.com>
2025-03-25 10:47:09 +0530

    25 march php

---
+++ loop5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a = array(1,2,3,4,5) ;
+    foreach ($a as $value) {
+        echo "Value is $value <br/>";
+    }
+    ?>
+</body>
+</html>
